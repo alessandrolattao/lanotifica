86f18705ac0c4b29cc8ba9a24a24d9ac9d6b43d3
Alessandro Lattao <me@alessandrolattao.com>
2025-12-10 23:07:30 +0100

    Fix deprecated kotlinOptions, migrate to compilerOptions DSL

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -45,7 +45,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "17" }
     buildFeatures { compose = true }
 
     lint {
@@ -84,6 +83,8 @@ android {
             )
     }
 }
+
+kotlin { compilerOptions { jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17) } }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
