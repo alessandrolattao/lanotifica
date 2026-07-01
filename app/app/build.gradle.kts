1b840556d0b09950b0a593f72cd3b87c89be1906
Alessandro Lattao <me@alessandrolattao.com>
2026-07-02 01:23:20 +0200

    chore: bump version to v1.2.4

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.alessandrolattao.lanotifica"
         minSdk = 34
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.2.3"
+        versionCode = 15
+        versionName = "1.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
